7ff6fcec7a2263b08f43ad168036f3dcdcc07de5
xFxdedFade <enderzaide@gmail.com>
2025-04-04 23:08:08 +0300

    feat: added header and langs

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,26 @@ import { React } from "react";
 function App() {
   return (
     <main>
-       Game goes here
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+        </header>
+        <ul className='langs'>
+          <li className='html'>HTML</li>
+          <li className='css'>CSS</li>
+          <li className='js'>Javascript</li>
+          <li className='react'>React</li>
+          <li className='ts'>Typescript</li>
+          <li className='node'>Node.js</li>
+          <li className='py'>Python</li>
+          <li className='ruby'>Ruby</li>
+          <li className='asm'>Assembly</li>
+        </ul>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
